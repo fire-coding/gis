f175f0c095c4238b4ac531545fae36405f48eb0c
Zerg <sv_shulga@uk.net>
2015-12-23 14:50:42 +0200

    Zerg

--- modules/map/js/map.js
+++ modules/map/js/map.js
@@ -19,7 +19,7 @@ var Map = function() {
 
   this.railZoomFinish = -1;
 
-  this.mapBounds = new OpenLayers.Bounds( 31.1841277,47.853281924,41.0602168317,52.5907424);
+  this.mapBounds = new OpenLayers.Bounds( 34.19,47.853281924,41.24,52.5907424);
 
   this.mapMinZoom = 6;
 
