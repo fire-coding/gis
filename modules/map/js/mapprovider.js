2b90ce4ca87197632a057ae38ef1ceea5abd1763
Zerg <sv_shulga@uk.net>
2015-12-25 15:41:08 +0200

    Zerg

--- modules/map/js/mapprovider.js
+++ modules/map/js/mapprovider.js
@@ -8,16 +8,24 @@ var MapProvider = {
   BOUNDS_UKRAINE: new OpenLayers.Bounds(3012305.420865983, 6182047.999176792, 3826050.8985648127, 6883730.558321303),
 
   scanexProvider: function(bounds) {
+    return this.provide(bounds, "gis_ua_web");
+  },
+
+  kosmoProvider: function(bounds) {
+    return this.provide(bounds, "gis_kosmo_ua");
+  },
+
+  provide: function(bounds, prefix) {
     var res = appMap.MapInstance.getResolution();
     var x = Math.round((bounds.left - this.maxExtent.left) / (res * this.tileSize.w));
-    var y=Math.round ((this.maxExtent.top - bounds.top) / (res * this.tileSize.h));
+    var y = Math.round ((this.maxExtent.top - bounds.top) / (res * this.tileSize.h));
     var z = this.map.getZoom();
     if (this.map.baseLayer.name == 'Virtual Earth Roads' || this.map.baseLayer.name == 'Virtual Earth Aerial' || this.map.baseLayer.name == 'Virtual Earth Hybrid') {
       z = z + 1;
     }
 
-    if (appMap.mapBounds.intersectsBounds( bounds ) && z >= appMap.mapMinZoom && z <= appMap.mapMaxZoom ) {
-      return appMap.tilesUrl + "gis_ua_web/" + z + "/" + x + "/" + y + "." +this.type;
+    if (appMap.mapBounds.intersectsBounds(bounds) && z >= appMap.mapMinZoom && z <= appMap.mapMaxZoom) {
+      return appMap.tilesUrl + prefix + "/" + z + "/" + x + "/" + y + "." + this.type;
     } else {
       return "/theme/images/none.png";
     }
