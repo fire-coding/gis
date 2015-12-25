7ff9630ef7ec717d39ac039483a12f8d8119e771
Zerg <sv_shulga@uk.net>
2015-12-25 15:53:52 +0200

    Zerg

--- modules/map/js/mapprovider.js
+++ modules/map/js/mapprovider.js
@@ -8,14 +8,14 @@ var MapProvider = {
   BOUNDS_UKRAINE: new OpenLayers.Bounds(3012305.420865983, 6182047.999176792, 3826050.8985648127, 6883730.558321303),
 
   scanexProvider: function(bounds) {
-    return MapProvider.provide(bounds, "gis_ua_web", this);
+    return MapProvider.provide(bounds, "gis_ua_web", "png", this);
   },
 
   kosmoProvider: function(bounds) {
-    return MapProvider.provide(bounds, "gis_kosmo_ua", this);
+    return MapProvider.provide(bounds, "gis_kosmo_ua", "jpg", this);
   },
 
-  provide: function(bounds, prefix, scope) {
+  provide: function(bounds, prefix, type, scope) {
     var res = appMap.MapInstance.getResolution();
     var x = Math.round((bounds.left - scope.maxExtent.left) / (res * scope.tileSize.w));
     var y = Math.round ((scope.maxExtent.top - bounds.top) / (res * scope.tileSize.h));
@@ -25,7 +25,7 @@ var MapProvider = {
     }
 
     if (appMap.mapBounds.intersectsBounds(bounds) && z >= appMap.mapMinZoom && z <= appMap.mapMaxZoom) {
-      return appMap.tilesUrl + prefix + "/" + z + "/" + x + "/" + y + "." + scope.type;
+      return appMap.tilesUrl + prefix + "/" + z + "/" + x + "/" + y + "." + type;
     } else {
       return "/theme/images/none.png";
     }
